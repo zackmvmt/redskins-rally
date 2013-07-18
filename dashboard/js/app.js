d16a741e505c1162eb92c6a81db36eb8ae2a479d
Zack Stickles <zack@movementstrategy.com>
2013-07-18 15:51:17 -0600

    end of the day commit

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,3 +1,46 @@
-$(function() {
-	console.log('hello world');
-});
+// dummy data to work off of
+var database = [
+	{ name: 'Charles Village Pub', city: 'Towson', state: 'Maryland' },
+	{ name: 'Sothern Sun', city: 'Boulder', state: 'Colorado' },
+	{ name: 'The Dark Horse', city: 'Boulder', state: 'Colorado' },
+	{ name: 'Kito\'s Cave', city: 'Petersburg', state: 'Alaska' }
+];
+
+function LocationModel(params) {
+
+	var self = this;
+
+	// data
+	// TODO...map values instead of hardcoding them
+	self.name = ko.observable(params.name);
+	self.city = ko.observable(params.city);
+	self.state = ko.observable(params.state);
+
+};
+
+function AppViewModel() {
+
+	var self = this;
+
+	// data
+	self.locs = ko.observableArray($.map(database, function(loc) {
+		return new LocationModel(loc);
+	}));
+	self.activeLoc = ko.observable();
+
+	// functions
+	self.removeLoc = function(loc) {
+		// TODO...server
+		self.locs.remove(loc);
+	}
+	self.editLoc = function(loc) {
+		self.activeLoc(loc);
+		$('.locationModal').modal('toggle');
+	}
+
+	// routes
+
+};
+
+// start up knockout
+ko.applyBindings(new AppViewModel());
