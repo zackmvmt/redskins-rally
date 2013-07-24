0e53bb5ff381076ef4c50e943c52b0d397918f56
Zack Stickles <zack@movementstrategy.com>
2013-07-24 17:11:37 -0600

    end of day commit

--- dashboard/api/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// include the config file and update the information (database name)
-	// idiorm is included with common and will be set up
-	// TODO, learn a little about how common and config work together
-	//			helper functions included in the common/global common/functions common/facebook.js
-
-	// database redskins_rally
-	// to add to config, client name and app name
-	require_once 'config.php';
