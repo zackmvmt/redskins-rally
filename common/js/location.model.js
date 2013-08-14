7aaa448a1a0ef4cf05f0b49a74da150f5caeee9b
Zack Stickles <zack@movementstrategy.com>
2013-08-14 10:14:31 -0600

    removed dummy data, moved around logic

--- common/js/location.model.js
+++ common/js/location.model.js
@@ -16,12 +16,24 @@ function LocationModel(params) {
 	self.state = ko.observable(params.state);
 
 	/* ********** **********
-	 * FUNCTIONS
+	 * DASHBOARD FUNCTIONS
 	 * ********** ********** */
 
 	// update information about the location
 	self.saveLoc = function() {
 		// TODO...server
+	};
+
+	// when the user clicks on the edit button, show the popup
+	self.editLoc = function() {
+		self.parent.activeLoc(self);
+		$('.locationModal').modal('toggle');
+	};
+
+	// when the user clicks on the delete button, remove this location
+	self.removeLoc = function() {
+		// TODO...server
+		self.parent.locs.remove(self);
 	};
 
 };
--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -1,36 +1,32 @@
 /* ********** **********
- * DUMMY DATABSE DATA
- * ********** ********** */
-var database = [
-	{ name: 'Charles Village Pub', city: 'Towson', state: 'Maryland' },
-	{ name: 'Sothern Sun', city: 'Boulder', state: 'Colorado' },
-	{ name: 'The Dark Horse', city: 'Boulder', state: 'Colorado' },
-	{ name: 'Kito\'s Cave', city: 'Petersburg', state: 'Alaska' }
-];
-
-
-/* ********** **********
  * FOREACHPROP
  * Custom handler that loops through an objects properties and
  * returns an array of key value pairs
  * NOTE: This is not fully abstracted out (see saveLoc)
  * ********** ********** */
 ko.bindingHandlers.foreachprop = {
+
+	// this isn't abstracted...the fields that are ignored
+	ignoredFields: ['saveLoc', 'editLoc', 'removeLoc', 'parent'],
+
 	getProps: function(obj) {
 		var props = [];
 		for (key in obj) {
 			// NOTE: the saveLoc check is not abstracted
-			if (obj.hasOwnProperty(key) && key != 'saveLoc')
+			var value = obj[key];
+			if (obj.hasOwnProperty(key) && this.ignoredFields.indexOf(key) == -1)
 				props.push({ key: key, value: obj[key] });
 		}
 		return props;
 	},
+
 	init: function(el, val) {
 		var value = ko.utils.unwrapObservable(val());
 		var props = ko.bindingHandlers.foreachprop.getProps(value);
 		ko.applyBindingsToNode(el, { foreach: props });
 		return { controlsDescendantBindings: true };
 	}
+
 }
 
 
@@ -46,9 +42,7 @@ function DashViewModel() {
 
 	var self = this;
 
-	self.locs = ko.observableArray($.map(database, function(loc) {
-		return new LocationModel(loc);
-	}));
+	self.locs = ko.observableArray();
 
 	self.activeLoc = ko.observable();
 
@@ -56,21 +50,27 @@ function DashViewModel() {
 	 * FUNCTIONS
 	 * ********** ********** */
 
-	self.removeLoc = function(loc) {
-		// TODO...server
-		self.locs.remove(loc);
-	};
-
-	self.editLoc = function(loc) {
-		self.activeLoc(loc);
-		$('.locationModal').modal('toggle');
-	};
-
+	// make a new location and trigger the popup
 	self.newLoc = function() {
 		// TODO...fix this
 		self.activeLoc = new LocationModel({});
 		$('.locationModal').modal('toggle');
 	};
+
+	// initialize function for pageload, grab the locations
+	$.ajax({
+		type: 'GET',
+		url: '../api/locations',
+		dataType: 'json',
+		success: function(data) {
+			// on success, add all the locations to the array
+			self.locs($.map(data, function(loc) {
+				var new_loc = new LocationModel(loc);
+				new_loc.parent = self;
+				return new_loc;
+			}));
+		}
+	});
 
 };
 
